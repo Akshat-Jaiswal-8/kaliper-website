b05b8ea20cb6808eeca01b3f9e9644a27fd5edd6
Sonu Kumar Mishra <mishrasonu9211@gmail.com>
2024-10-08 12:33:08 +0530

    footer added

--- app/(home)/_components/footer.tsx
+++ app/(home)/_components/footer.tsx
@@ -1,3 +1,4 @@
+import Link  from "next/link";
 import React from "react";
 
 export const Footer = (): React.ReactNode => {
@@ -5,10 +6,10 @@ export const Footer = (): React.ReactNode => {
     <section className={"mx-auto h-full w-[90%] rounded-xl bg-[#1012144D] bg-opacity-30 p-2 px-6"}>
       <h1 className={"mb-10 text-center text-2xl font-light"}>Follow Us</h1>
       <h1 className={"flex justify-center gap-x-4 text-3xl font-normal"}>
-        <span>Linkedin</span>
-        <span>Instagram</span>
-        <span>Facebook</span>
-        <span>Twitter</span>
+        <Link href={'https://www.linkedin.com/company/kaliper/'}>Linkedin</Link>
+        <Link href={'https://www.instagram.com/kalipertech/'}>Instagram</Link>
+        <Link href={'https://www.facebook.com/'}>Facebook</Link>
+        <Link href={'https://x.com/home?lang=en'}>X</Link>
       </h1>
       <div className={"my-10"}>
         <p className={"mb-4 font-inter text-xs font-semibold text-[#ECECEC] text-opacity-60"}>
@@ -23,17 +24,17 @@ export const Footer = (): React.ReactNode => {
       </div>
       <div className={"flex w-full items-center justify-between"}>
         <p className={"w-[15%] font-inter text-sm font-semibold"}>
-          1901 Thornridge Cir. Shiloh, Hawaii 81063
+        Plot No 38, Bhagwan Das, Alok Puri Kalyanpur Lko, Kalyanpur(Lucknow), Uttar Pradesh India 226022
         </p>
         <div className={"flex flex-col gap-y-2 text-xs"}>
           <button
             className={
               "rounded-lg border bg-white py-2 font-inter text-sm font-semibold text-black"
             }>
-            Request a call
+           <a href="tel:+918299513060"> Request a call</a>
           </button>
-          <p className={"font-inter text-sm font-semibold"}>+1 (999) 999-99-99</p>
-          <p>info@logoipsum.com</p>
+          <p className={"font-inter text-sm font-semibold"}>+918299513060</p>
+          <a href="mailto:amit.verma@kaliper.in">amit.verma@kaliper.in</a>
         </div>
       </div>
     </section>
